36b114e2eb69c5745b90b9e90362ac19572ee290
MaksimSelin <maksimselin108@gmail.com>
2022-09-07 22:19:06 +0300

    Add project files.

---
+++ UdemiTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace UdemiTest
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [Fact]
+        public void Test1()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(new Uri("http://localhost:5001/"));
+            driver.FindElement(By.LinkText("Product")).Click();
+            driver.FindElement(By.LinkText("Create")).Click();
+        }
+    }
+}
